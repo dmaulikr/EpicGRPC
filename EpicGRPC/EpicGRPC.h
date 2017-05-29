93647e9923f68fd6dab8b63c50c81954052dc665
Al Hennessey <alhennessey92@gmail.com>
2017-05-29 11:46:16 +0100

    Initial Commit

---
+++ EpicGRPC/EpicGRPC.h
@@ -0,0 +1,19 @@
+//
+//  EpicGRPC.h
+//  EpicGRPC
+//
+//  Created by Al Hennessey on 29/05/2017.
+//  Copyright © 2017 Epic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EpicGRPC.
+FOUNDATION_EXPORT double EpicGRPCVersionNumber;
+
+//! Project version string for EpicGRPC.
+FOUNDATION_EXPORT const unsigned char EpicGRPCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EpicGRPC/PublicHeader.h>
+
+
